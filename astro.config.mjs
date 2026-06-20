84738fed260992417eae4c4dde33590c8e8444de
Argy Anggara <162962899+4RGY@users.noreply.github.com>
2026-06-20 10:15:18 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://angg4ra.my.id',
+
+  integrations: [
+    sitemap(),
+  ],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   redirects: {
-    '/': '/id/'
+    '/': '/id/',
   },
+
   i18n: {
     defaultLocale: 'id',
     locales: ['id', 'en'],
